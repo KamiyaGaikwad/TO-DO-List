fd4ce0d64c535372315e89a4460cc65b0a322a9c
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2021-11-03 16:30:13 +0530

    feat : add select functionality

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 var todoInput = document.querySelector(".todo-input")
 var todoButton = document.querySelector(".todo-button")
 var todoList = document.querySelector(".todo-list")
-
+var filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click",addTodo)
 todoList.addEventListener("click",deleteCheck);
+filterOption.addEventListener("click",filterTodo);
 
 //Functions
 function addTodo(event){
@@ -52,4 +53,31 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach((todo) => {
+            switch (e.target.value) {
+                case "all":
+                    todo.style.display = "flex";
+                    break;
+                case "completed":
+                    if (todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    }
+                    else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "uncompleted":
+                    if (!todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    }
+                    else {
+                        todo.style.display = "none";
+                    }
+                    break;
+            }
+        });
 }
